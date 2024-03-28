2b72dc6221a1d6bd88070a362632fdf013f7efaa
g1ann0z <giannozcydia@gmail.com>
2024-03-28 17:42:25 +0100

    reverseStringAllSolution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,15 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
+//mia
+/* function reverse(str) {
     let currentVerse = str.split("");
     let reversed = [];
     for(let i = currentVerse.length-1; i >= 0; i-- ){
         reversed.push(currentVerse[i]);
     }
     return reversed.join("")
+} */
+
+//come la mia ma con FOR OF
+/* function reverse(str) {
+    let reversed = '';
+    for (let carattere of str){
+        reversed = carattere + reversed;
+    }
+    return reversed;
+} */
+
+//alternativa più semplice
+/* function reverse(str) {
+    return str
+    .split("")
+    .reverse()
+    .join("");
+} */
+
+//più complessa con reduce()
+function reverse(str) {
+    return str.split('').reduce((reversed, carattere) => carattere + reversed, '');
 }
-
-
-
 module.exports = reverse;
