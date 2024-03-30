cbb55a0db614c241e7357c0e8d53e6116ac9abe8
g1ann0z <giannozcydia@gmail.com>
2024-03-30 17:29:29 +0100

    reverseInt e fizzbuzz

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,21 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let stringNumber = n
+        .toString()
+        .split("")
+        .reverse()
+        .join("");
+
+    
+        if (n < 0){
+            return parseInt(stringNumber) * -1;
+        }
+        return parseInt(stringNumber);
+
+}
+
+console.log(reverseInt(123));
 
 module.exports = reverseInt;
