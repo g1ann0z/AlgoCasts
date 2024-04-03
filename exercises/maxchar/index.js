f08037d9d22388a16395b4e37e1846c4031ed39e
g1ann0z <giannozcydia@gmail.com>
2024-04-03 13:51:34 +0200

    maxChar_done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,37 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+
+
+function maxChar(str) {
+    let strToObj = {};
+    for (const char of str) {
+        if (!strToObj[char]){
+            strToObj[char] = 1;
+        } else {
+            strToObj[char]++;
+        }
+    }
+    let maxValue = 0;
+    let maxChar = '';
+
+    for (let value in strToObj){
+        if (strToObj[value] > maxValue){
+            maxValue = strToObj[value];
+            maxChar = value;
+        }
+    }
+    
+    return maxChar;
+
+}
+
+
+
+
+
+/* for (let key of arrayDiChiavi){
+    console.log(key);
+} */
 
 module.exports = maxChar;
