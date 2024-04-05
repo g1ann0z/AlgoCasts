e17a9476cf5caf43b3a714e8565c8985209d33f8
g1ann0z <giannozcydia@gmail.com>
2024-04-05 19:44:02 +0200

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,55 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+//easymode
+/* function anagrams(stringA, stringB) {
+    if (stringA.length !== stringB.length){
+        return false;
+    }
+    let orderedStringA = stringA.split('').sort().join('');
+    let orderedStringB = stringB.split('').sort().join('');
+    if (orderedStringA === orderedStringB){
+        return true;
+    }
+    return false;
+} */
+
+//test con mapping delle lettere
+function anagrams(stringA, stringB) {
+    if (stringA.length !== stringB.length){
+        return false;
+    }
+    let orderedStringA = stringA.replace(/[^\w]/g, "").toLowerCase(); //rimuove tutti i caratteri non parole della stringa a livello globale e li sostituisce con "" 
+    let orderedStringB = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+    let objStringA = {};
+    let objStringB = {};
+
+    for(let charA of orderedStringA){
+        if(objStringA[charA]){
+            objStringA[charA]++;
+        } else {
+            objStringA[charA] = 1;
+        }
+    }
+    
+
+    for(let charB of orderedStringB){
+        if(objStringB[charB]){
+            objStringB[charB]++;
+        } else {
+            objStringB[charB] = 1;
+        }
+    }
+    
+    let check = true;
+    
+    for(let char in objStringA){
+        if (objStringA[char] !== objStringB[char]){
+            check = false;
+        }
+        }
+    return check;
+}
 
 module.exports = anagrams;
