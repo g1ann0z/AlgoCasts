0ebf2b6c39fa9bad93763e5900c1eac606e79f0a
g1ann0z <giannozcydia@gmail.com>
2024-04-16 19:04:38 +0200

    linkedList_removeLast

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -3,21 +3,80 @@
 // See 'directions' document
 
 class Node {
-    constructor(data, next){
+    constructor(data, next) {
         this.data = data;
         this.next = (next !== undefined) ? next : null;
     } //altrimenti this.next = next; ma (next = null) nell'argomento
 }
 
 class LinkedList {
-    constructor(){
+    constructor() {
         this.head = null;
     }
 
-    insertFirst(data){
+    insertFirst(data) {
         this.head = new Node(data, this.head);
 
     }
+
+    size() {
+        let counter = 0;
+        let node = this.head;
+        while (node) {
+            counter++
+            node = node.next;
+        }
+        return counter;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        let node = this.head;
+
+        if (!this.head) {
+            return null;
+        } else {
+            while(node.next){
+                node = node.next;
+            }
+            return node;
+        }
+    }
+
+    clear(){
+        this.head = null;
+    }
+
+    removeFirst(){
+        if(!this.head){
+            return;
+        }
+        let node = this.head;
+        this.head = node.next;
+    }
+
+    removeLast(){
+
+        if(!this.head){
+            return;
+        } else if (!this.head.next){
+            this.head = null;
+            return;
+        }
+
+        let previus = this.head;
+        let node = this.head.next;
+        while(node.next){
+            previus = previus.next;
+            node = node.next;
+        }
+        previus.next = null;
+    }   
+
 }
+
 
 module.exports = { Node, LinkedList };
